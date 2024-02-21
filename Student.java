2617ba4601c3f7322d29db62fee4173068a9a000
aravind <aravindemails@gmail.com>
2024-02-21 22:46:58 +0530

    StudentClassforaverage

---
+++ Student.java
@@ -0,0 +1,30 @@
+public class Student {
+
+
+    String name;
+    int marks;
+    int count;
+
+
+     public Student(String name, Integer marks,int count) {
+        this.name = name;
+        this.marks = marks;
+        this.count = count;
+
+    }
+
+
+    @Override
+    public String toString() {
+        return "Student [name=" + name + ", marks=" + marks + ", count=" + count + "]";
+    }
+
+    
+    
+
+
+
+
+
+    
+}
