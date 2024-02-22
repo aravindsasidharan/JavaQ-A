6adc1b88d9c128a35de174b45a21a6e155d77060
aravind <aravindemails@gmail.com>
2024-02-22 14:26:33 +0530

    Added morgan unfinished

---
+++ SequenceMorgan.java
@@ -0,0 +1,84 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class SequenceMorgan {
+
+
+
+    public static void main(String args[])
+    {
+
+      String input = "1 2 3 5 9 a 6 7 8 4 @ -5 -7 -3 -2 -1";
+
+      String[] splitInut = input.split(input);
+
+      List<Integer> sequence = new ArrayList<>();
+
+      List<List<Integer>> sequences = new ArrayList<>();
+
+      Integer previous = null;
+
+      for(String s: splitInut)
+      {
+System.out.println(s);
+        try{
+
+            Integer current = Integer.parseInt(s);
+            System.out.println(current);
+            if(null != previous)
+            {
+                if((current - previous) == 1)
+                {
+                    sequence.add(current);
+                }
+                else
+                {
+                    if(sequence.size() > 1)
+                    {
+                        sequences.add(sequence);
+
+                    }
+                    sequence = new ArrayList<>();
+                    sequence.add(current);
+                }
+                previous = current;
+            }
+            else
+            {
+                previous = current;
+                sequence.add(current);
+            }
+        }
+        catch(NumberFormatException e)
+        {
+          if(sequence.size() > 1)
+          {
+            sequences.add(sequence);
+          }
+          previous = null;
+
+          sequence = new ArrayList<>();
+        }
+    }
+    if(sequence.size() > 1)
+    {
+        sequences.add(sequence);
+    }
+    sequences.stream().forEach(System.out::println);
+}
+
+        }
+    
+      
+
+      
+
+
+
+    
+
+
+
+
+    
+
